4a5877e9ff4f5a7b108cf6643746cc8df6809f77
Shaun Bulbrook <sbulbrook@emea.corp.jwt.com>
2018-08-10 15:50:53 +0100

    Supdates

--- socket-tut-chat-input/src/App.js
+++ socket-tut-chat-input/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import io from 'socket.io-client';
 
@@ -97,10 +96,7 @@ class ChatInput extends React.Component {
 	 */
 	handleMessageKeyPress(event) {
 		if(event.key === 'Enter') {
-			this.socket.emit('new message authored', {
-				author: this.state.author,
-				message: event.target.value,
-			});
+			//
 			event.target.value = '';
 		}
 	}
